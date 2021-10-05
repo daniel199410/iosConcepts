3d80d74509225bafd6fa810b517017d69d6c2501
dcatanor <dasniel199410@gmail.com>
2021-10-05 11:32:28 -0500

    Creación de playground

---
+++ Ejemplo/Ejemplo/MyPlayground.playground/Pages/booleans.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+var cedulaProfesional = false
+var salario: Int
+if cedulaProfesional {
+    salario = 9000
+} else {
+    salario = 8000
+}
+salario
---
+++ Ejemplo/Ejemplo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejemplo
+//
+//  Created by Daniel Cataño Restrepo on 4/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
