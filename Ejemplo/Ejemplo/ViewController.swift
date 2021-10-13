435b45fba122a499c63f4a4c67b53f26b10da772
dcatanor <dasniel199410@gmail.com>
2021-10-13 16:15:42 -0500

    Pickers, buttons and labels

--- Ejemplo/Ejemplo/ViewController.swift
+++ Ejemplo/Ejemplo/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var picker: UIPickerView!
+    
+    private let pickerValues = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.dataSource = self
+        picker.delegate = self
     }
 
-
+    
+    @IBAction func button(_ sender: Any) {
+        if label.text == "Sí" {
+            label.text = "No"
+        } else {
+            label.text = "Sí"
+        }
+    }
+    
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = pickerValues[row]
+    }
+}
